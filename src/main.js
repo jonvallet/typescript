7007a9903f212ed934670acb7e8d1362fd612438
j.vallet <j.vallet@gmail.com>
2018-05-01 16:16:21 +0100

    Examples

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
 const repositoryJS = require('./repositoryJS');
+//Table name properties.property, id: 325820
 
-const params = {};
-
-repositoryJS.getItem(params);
--- src/mainTS.ts
+++ src/mainTS.ts
@@ -1,10 +1,3 @@
 import repositoryTS from "./repositoryTS";
 import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
-
-const params: DocumentClient.GetItemInput = {
-    TableName: 'table.name',
-    Key: {id: 'id'}
-};
-
-repositoryTS.getItem(params);
-
+//Table name properties.property, id: 325820
